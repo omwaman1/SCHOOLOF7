7ee7a7953b62a4026e3ae19a6e266942485e4723
omwaman1 <omwaman1@github.com>
2026-01-08 22:20:36 +0530

    style: Add Oswald font with green color to 'Your Masterclass To STARTUP' title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Startup Masterclass | School of 7</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&family=Barlow+Condensed:wght@500;600;700;800&family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&family=Barlow+Condensed:wght@500;600;700;800&family=Luckiest+Guy&family=Oswald:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 
