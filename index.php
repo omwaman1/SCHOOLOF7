7b7d174af3e30c16c3f74aca143212b403c25660
omwaman1 <omwaman1@gmail.com>
2026-01-23 20:00:15 +0530

    Fix thumbnail filename - use underscores instead of spaces

--- index.php
+++ index.php
@@ -196,7 +196,7 @@
         <h2 class="video-help-title">How This Course Will Help You?</h2>
         <div class="video-container-framed">
             <div class="video-wrapper">
-                <video muted preload="none" id="coursePreviewVideo" playsinline controls poster="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/thumbnail%20video%20image.png"></video>
+                <video muted preload="none" id="coursePreviewVideo" playsinline controls poster="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/thumbnail_video_image.png"></video>
             </div>
             <h3 class="video-caption" style="color: #67635f; margin-top: 20px;">" Don't Start a Business Blind - Buy the Map "</h3>
         </div>
