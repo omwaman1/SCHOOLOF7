26af8ed457fbedda0bd3f6872a220a52474918e0
vividhjain10-ship-it <vividhjain10@gmail.com>
2026-02-25 13:36:09 +0530

    Corporate employees, business owners name change
    Corporate employees, business owners name change

--- index.php
+++ index.php
@@ -348,14 +348,14 @@
             <button class="audience-nav-btn prev" id="audiencePrev">‹</button>
             <div class="audience-grid" id="audienceCarousel">
                 <div class="audience-card">
-                    <img src="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/1.png" alt="Entrepreneurs">
+                    <img src="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/1.png" alt="Corporate Employees">
                     <div class="audience-info">
                         <h4> ENTREPRENEURS</h4>
                         <p> business owners looking to scale online</p>
                     </div>
                 </div>
                 <div class="audience-card">
-                    <img src="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/2.png" alt="Freelancers">
+                    <img src="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/2.png" alt="Business Owners">
                     <div class="audience-info">
                         <h4> FREELANCERS</h4>
                         <p> wanting to upskill & charge more</p>
@@ -369,7 +369,7 @@
                     </div>
                 </div>
                 <div class="audience-card">
-                    <img src="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/4.png" alt="Creatives">
+                    <img src="https://pub-29a82b1a8c4f45c1a62aa880ed0adcc0.r2.dev/4.png" alt="Freelancers">
                     <div class="audience-info">
                         <h4> CREATIVES</h4>
                         <p> artists wanting to sell their work online</p>
