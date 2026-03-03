73a4b9285e20f1a03970bd9d68af2e984bceb14b
vividhjain10-ship-it <vividhjain10@gmail.com>
2026-03-03 11:10:52 +0530

    learn fast execute faster.
    learn fast execute faster.

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
             </button>
         </div>
         <div class="hero-content">
-            <h1 class="hero-title">Learn Fast<br>Execute Faster</h1>
+            <h1 class="hero-title">Learn Fast<br>Execute Faster.</h1>
             <p class="hero-subtitle">The ultimate guide to scaling your business online.
             </p>
             <div class="hero-actions">
