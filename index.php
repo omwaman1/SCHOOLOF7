7a8a087cccef88ddcc0f41e7433d4813cfb71057
webtickupptick <webtickupptick@gmail.com>
2026-03-26 10:52:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -531,7 +531,7 @@
                     <li class="check">Course Upgrades at Zero Cost</li>
                     <li class="check">Certificate of Achievement</li>
                 </ul>
-                <button onclick="initiatePayment199(100, 'Demo Masterclass', 'The-Complete-Guide-To-Starting-Up')" class="btn btn--blue-brand">Enroll Now</button>
+                <button onclick="initiatePayment199(100, 'Demo Masterclass', 'The-Complete-Guide-To-Starting-Up')" class="btn btn--blue-brand">Enroll Now!</button>
                 <p class="refund-policy">Founder Ready in 8 Modules</p>
             </div>
         </div>
